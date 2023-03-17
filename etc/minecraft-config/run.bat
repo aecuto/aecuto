d353c040e5994e6d732c0e78423d73fd18569c1f
Kampee Tonoi <kampee.expertly01@gmail.com>
2023-03-18 01:03:11 +0700

    Update run.bat

--- etc/minecraft-config/run.bat
+++ etc/minecraft-config/run.bat
@@ -6,12 +6,10 @@ REM  pass them to this script directly
 
 taskkill /f /im ngrok.exe 
 
-START /b cmd /c ngrok tcp -config=./ngrok.yml 25565
+START /b cmd /c ngrok tcp --config ./ngrok.yml 25565
 
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.2-43.1.1/win_args.txt %* nogui
+java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.3-44.1.0/win_args.txt %* nogui
 
 taskkill /f /im ngrok.exe 
 
 pause
-
-
